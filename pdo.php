1914105fdc1ab39bd272477e469e67a0bd435001
nicolaslaing <34254569+nicolaslaing@users.noreply.github.com>
2018-03-08 00:25:08 -0500

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -5,23 +5,23 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors' , 1);
 
 $user = "nal9";
-$project = "nal9";
 $password = "Movlksomh123";
 $host = "sql1.njit.edu";
 
-$db = mysqli_connect($host, $user, $password, $project);
+$dsn = "mysql:host=$host;dbname=$user";
 
-if (mysqli_connect_errno())
-  {
-	  echo "<h1>Failed to connect to MySQL: " . mysqli_connect_error();
-	  exit();
-  }
-print "<h1>Successfully connected to MySQL.</h1><br />";
+try {
+    $conn = new PDO($dsn, $user, $password);
+    echo "Connected successfully<br>";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+$conn = null;
 
-mysqli_select_db($db, $project );
+//mysqli_select_db($conn, $project );
 //****************************************************************************************
 $s = "SELECT * FROM accounts WHERE id < 6";
-$result = mysqli_query($db, $s);
+$result = prepare($s);
 
 $numRows = mysqli_num_rows($result);
 echo "Results: $numRows";
